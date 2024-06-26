568f5d5678a9e27cf94a7f052d1cba648cc3affd
Supratit Datta <supratitdatta@gmail.com>
2024-06-26 22:29:49 +0530

    Design Updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
 import Search from './Search';
+import icon from "../assets/icon.png";
 import '../css/header.css';
 
 const Header = ({ title }) => {
   return (
     <div className='header-container'>
       <header className='header-content'>
-        <h1 className='header-title'>
-          <FontAwesomeIcon icon={faNewspaper} className='header-icon' />
-          {title}
-        </h1>
+        <div className="header-pic">
+          <img src={icon} className="header-logo"></img>
+          <div></div>
+          <h1 className='header-title'>
+            {/* <FontAwesomeIcon icon={faNewspaper} className='header-icon' /> */}
+            {/* {title} */}
+          </h1>
+        </div>
         <div className='header-search'>
           <Search />
         </div>
--- src/constants/index.js
+++
@@ -1,9 +0,0 @@
-export const API_KEY = "10665175db41441ba4f4c8b8007dd2b4";
-export const BASE_URL = "https://newsapi.org/v2/";
-
-// 10665175db41441ba4f4c8b8007dd2b4 bishal roy
-// 222441375ed541dab0424422e3a3147f supratit datta
-// f4ef41ea42a143d289642fff3089284b datta supratit
-// const API_KEY = '10665175db41441ba4f4c8b8007dd2b4'
-// const key4 = '2d55f494fe674381af5e990d5d995b6e'
-// const key5 = 'ac009e2e2d1b4cc3a6ec7087c51a73af'
